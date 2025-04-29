e9678931effa9a5f6494545c74bc3a7f8d2100d6
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-04-29 16:25:39 -0700

    style layout | enable chat only with getServerSession

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import Chat from "@/app/components/Chat";
+import { Separator } from "@/components/ui/separator";
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+  const session = await getServerSession();
+
   return (
     <main className="p-5">
       <h1 className="text-4xl font-bold">Welcome To GPT Chat</h1>
+      {!session?.user?.name && (
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-2xl">Please sign in to continue</p>
+        </div>
+      )}
+      {session?.user?.name && (
+        <>
+          <Separator className="my-5" />
+          <Chat />
+        </>
+      )}
     </main>
   );
 }
